844c171db327d66a56ec2d59b397c2404ff73737
TheRealKamisama <1141946313@qq.com>
2018-03-24 16:10:12 +0800

    也就那样吧

---
+++ src/main/java/com/github/therealkamisama/shitmixer/crafting/CraftingLoader.java
@@ -0,0 +1,35 @@
+package com.github.therealkamisama.shitmixer.crafting;
+
+import com.github.therealkamisama.shitmixer.Item.ItemLoader;
+import com.github.therealkamisama.shitmixer.block.BlockLoader;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class CraftingLoader
+{
+    public CraftingLoader()
+    {
+        registerRecipe();
+        registerSmelting();
+        registerFuel();
+    }
+
+    private static void registerRecipe()
+    {       GameRegistry.addShapedRecipe(new ItemStack(BlockLoader.blockShit), new Object[]
+            {
+
+            });
+
+    }
+
+    private static void registerSmelting()
+    {
+
+    }
+
+    private static void registerFuel()
+    {
+
+    }
+}
