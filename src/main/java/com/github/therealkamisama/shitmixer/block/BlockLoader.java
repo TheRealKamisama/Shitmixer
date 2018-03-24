8303b76b2bc09302183373978b212b523633d160
TheRealKamisama <1141946313@qq.com>
2018-03-24 13:40:51 +0800

    屎方块写完了

--- src/main/java/com/github/therealkamisama/shitmixer/block/BlockLoader.java
+++ src/main/java/com/github/therealkamisama/shitmixer/block/BlockLoader.java
@@ -1,8 +1,14 @@
 package com.github.therealkamisama.shitmixer.block;
 
+
 import net.minecraft.block.Block;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockLoader
 {
@@ -10,11 +16,24 @@ public class BlockLoader
 
     public BlockLoader(FMLPreInitializationEvent event)
     {
-        register(blockShit, "BlockShit");
+        register(blockShit, "blockShit");
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void registerRenders()
+    {
+        registerRender(blockShit);
     }
 
     private static void register(Block block, String name)
     {
         GameRegistry.registerBlock(block.setRegistryName(name));
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void registerRender(Block block)
+    {
+        ModelResourceLocation model = new ModelResourceLocation(block.getRegistryName(), "inventory");
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, model);
     }
 }
--- src/main/java/com/github/therealkamisama/shitmixer/common/CommonProxy.java
+++ src/main/java/com/github/therealkamisama/shitmixer/common/CommonProxy.java
@@ -1,6 +1,7 @@
 package com.github.therealkamisama.shitmixer.common;
 
 import com.github.therealkamisama.shitmixer.Item.ItemLoader;
+import com.github.therealkamisama.shitmixer.block.BlockLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -10,6 +11,7 @@ public class CommonProxy
     public void preInit(FMLPreInitializationEvent event)
     {
         new ItemLoader(event);
+        new BlockLoader(event);
     }
 
     public void init(FMLInitializationEvent event)
