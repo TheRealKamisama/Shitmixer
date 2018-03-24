a9c63e8690650b65a3e9afe04519780df763b1c5
TheRealKamisama <1141946313@qq.com>
2018-03-24 22:02:12 +0800

    创造模式物品栏（还没写完

--- src/main/java/com/github/therealkamisama/shitmixer/common/CommonProxy.java
+++ src/main/java/com/github/therealkamisama/shitmixer/common/CommonProxy.java
@@ -3,6 +3,7 @@ package com.github.therealkamisama.shitmixer.common;
 import com.github.therealkamisama.shitmixer.Item.ItemLoader;
 import com.github.therealkamisama.shitmixer.block.BlockLoader;
 import com.github.therealkamisama.shitmixer.crafting.CraftingLoader;
+import com.github.therealkamisama.shitmixer.creativetab.CreativeTabsLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -11,6 +12,7 @@ public class CommonProxy
 {
     public void preInit(FMLPreInitializationEvent event)
     {
+        new CreativeTabsLoader(event);
         new ItemLoader(event);
         new BlockLoader(event);
     }
