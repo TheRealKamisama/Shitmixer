19d5c53544bfff69bd6a01dbe0ebe8dbb136158d
TheRealKamisama <1141946313@qq.com>
2018-04-02 23:34:50 +0800

    renamed

--- src/main/java/com/github/therealkamisama/shitmixer/block/BlockLoader.java
+++ src/main/java/com/github/therealkamisama/shitmixer/block/BlockLoader.java
@@ -3,18 +3,13 @@ package com.github.therealkamisama.shitmixer.block;
 
 import com.github.therealkamisama.shitmixer.common.register.RegisterManager;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 
-public class BlockLoader
-{
+public class BlockLoader {
     public static Block blockShit = new BlockShit();
 
-    public BlockLoader()
-    {
-        RegisterManager.getInstance().putRegister(Item.getItemFromBlock(blockShit));
+    public BlockLoader() {
         RegisterManager.getInstance().putRegister(blockShit);
+        RegisterManager.getInstance().putRegister(new ItemBlock(blockShit).setRegistryName("block_shit"));
     }
-
-
 }
