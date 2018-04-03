4b72359b9a8f2f548f2f61e022fc373e27b5b178
Cyl18 <cyl18a@gmail.com>
2018-04-03 23:00:50 +0800

    fix a stupid bug

--- src/main/java/com/github/therealkamisama/shitmixer/ShitDroppingHandler.java
+++ src/main/java/com/github/therealkamisama/shitmixer/ShitDroppingHandler.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -24,7 +25,7 @@ public class ShitDroppingHandler {
 
     @SubscribeEvent
     public void shitDropTiming(TickEvent.WorldTickEvent event) {
-        final long ticks = event.world.getTotalWorldTime();
+        final long ticks = event.world.getWorldTime();
         if (ticks % ConfigLoader.getShitGenerateTime() == 0) {
             event.world.getEntities(EntityAnimal.class, entity -> true).forEach(entity ->
                     entity.entityDropItem(new ItemStack(shit), 1));
